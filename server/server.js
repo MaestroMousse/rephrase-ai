387180624cde1a06acc4898ac3764a9ef4414a8e
outpvt1 <echoiniquity2@gmail.com>
2025-10-21 00:40:28 +0300

    Add test route and confirm backend server running

--- server/server.js
+++ server/server.js
@@ -1,5 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 dotenv.config();
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello from the other side!!");
+});
